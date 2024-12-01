98eddb44f97f7b783d0eedd0cb5487cd9612b38c
Dattoon <ttmdvhd@gmail.com>
2024-12-01 15:20:51 +0700

    fix khoa hoc

--- WebHocTap.Web/Common/AppExtension.cs
+++ WebHocTap.Web/Common/AppExtension.cs
@@ -86,5 +86,24 @@ namespace WebHocTap.Web.Common
             }
             return false;
         }
+
+
+        
+            public static bool IsAjaxRequest(this HttpRequest request)
+            {
+                if (request == null)
+                {
+                    throw new ArgumentNullException(nameof(request));
+                }
+
+                if (request.Headers != null)
+                {
+                    return request.Headers["X-Requested-With"] == "XMLHttpRequest";
+                }
+
+                return false;
+            }
+        
+
     }
 }
