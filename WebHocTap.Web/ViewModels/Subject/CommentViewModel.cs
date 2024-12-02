7e3fed4c768d40e36fd154d3f0248e52b2de16b9
Dattoon <ttmdvhd@gmail.com>
2024-12-02 16:46:48 +0700

    fix

--- WebHocTap.Web/ViewModels/Subject/CommentViewModel.cs
+++ WebHocTap.Web/ViewModels/Subject/CommentViewModel.cs
@@ -6,7 +6,7 @@
         public string Content { get; set; }
         public int LessonId { get; set; }
         public int UserId { get; set; }
-        public string UserName { get; set; } // Bổ sung UserName
+        public string UserName { get; set; } 
         public DateTime CreatedAt { get; set; }
     }
 }
